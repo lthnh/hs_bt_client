c97108e726a2055c095482f6d90af1394325df42
lthnh <imr3ng4l@gmail.com>
2024-09-02 22:52:46 +0700

    test run pass, able to receive signal on two characteristics at a time

--- bt_client_v2.py
+++ bt_client_v2.py
@@ -4,16 +4,21 @@ import time
 import threading
 from queue import Queue, Empty
 from collections import deque
+from enum import Enum
 from typing import Union
 
 import dearpygui.dearpygui as dpg
 from bleak import BleakScanner, BleakClient, BLEDevice, BleakGATTCharacteristic
-import numpy as np
 
 SERVER_NAME = "ESP_SPP_SERVER"
 HS_CHAR_UUID = "cdd4c6c4-7a3c-599b-324e-f93750d2f002"
+BP_CHAR_UUID = "cdd4c6c4-7a3c-599b-324e-f93750d2f003"
 VREF = 5.0
 MAX_DATA_LEN = 200
+
+class BIOSIG(Enum):
+    HS = 0
+    BP = 1
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -21,7 +26,7 @@ logging.basicConfig(
     format="%(asctime)-15s %(name)-8s %(levelname)s: %(message)s",
 )
 
-def receive_data(queue: Queue):
+def receive_data(queue: Queue, stop_event: threading.Event):
     async def discover_server() -> Union[BLEDevice, None]:
         server = None
         devices = await BleakScanner.discover()
@@ -67,14 +72,28 @@ def receive_data(queue: Queue):
         hs_char = None
         for service in client.services:
             for char in service.characteristics:
-                if HS_CHAR_UUID == char.uuid:
+                if char.uuid == HS_CHAR_UUID:
                     hs_char = char
         return hs_char
 
-    def notification_callback(char: BleakGATTCharacteristic, data: bytearray):
+    async def find_bp_service(client: BleakClient) -> Union[BleakGATTCharacteristic, None]:
+        bp_char = None
+        for service in client.services:
+            for char in service.characteristics:
+                if char.uuid == BP_CHAR_UUID:
+                    bp_char = char
+        return bp_char
+
+    async def notification_callback(char: BleakGATTCharacteristic, data: bytearray):
         val = int.from_bytes(bytes=data, byteorder='little', signed=False)
-        queue.put_nowait(val)
-        logger.info(f"{data} {val} {(val / 4096.0 * VREF):.3f}");
+        if char.uuid == HS_CHAR_UUID:
+            sig_type = BIOSIG.HS
+        elif char.uuid == BP_CHAR_UUID:
+            sig_type = BIOSIG.BP
+        else:
+            sig_type = None
+        queue.put_nowait((sig_type, val))
+        logger.info(f"{sig_type} {data} {val} {(val / 4096.0 * VREF):.3f}");
 
     async def main():
         server = await discover_server()
@@ -88,36 +107,43 @@ def receive_data(queue: Queue):
             logger.info("begin to extract all of its services")
             await extract_all_services(client)
             hs_char = await find_hs_service(client)
+            bp_char = await find_bp_service(client)
             await client.start_notify(hs_char, notification_callback)
-            while True:
+            await client.start_notify(bp_char, notification_callback)
+            while not stop_event.is_set():
                 if not client.is_connected:
                     break
                 await asyncio.sleep(1)
 
     asyncio.run(main())
 
-queue = Queue()
-start_time = time.perf_counter()
-time_data = deque(maxlen=MAX_DATA_LEN)
-hs_data = deque(maxlen=MAX_DATA_LEN)
-bp_data = deque(maxlen=MAX_DATA_LEN)
+def update_plot(queue: Queue, stop_event: threading.Event):
+    start_time = time.perf_counter()
+    time_data = deque(maxlen=MAX_DATA_LEN)
+    hs_data = deque(maxlen=MAX_DATA_LEN)
+    bp_data = deque(maxlen=MAX_DATA_LEN)
+    while not stop_event.is_set():
+        try:
+            sig_type, val = queue.get_nowait()
+            # logger.info(f'get {val}')
+        except Empty:
+            continue
+        time_data.append(time.perf_counter() - start_time)
+        hs_data.append(val)
+        data_x = list(time_data)
+        if sig_type == BIOSIG.HS:
+            hs_data_y = [x / 4096.0 * VREF for x in list(hs_data)]
+            dpg.configure_item('heart_sound', x=data_x, y=hs_data_y)
+        if sig_type == BIOSIG.BP:
+            bp_data_y = [x / 4096.0 * VREF for x in list(bp_data)]
+            dpg.configure_item('blood_pressure', x=data_x, y=hs_data_y)
+        # logger.info('set value successfully')
+        if dpg.get_value('auto_fit_checkbox'):
+            dpg.fit_axis_data('x_axis')
+            # dpg.set_axis_limits_auto('y_axis')
 
-def update_plot():
-    global queue, start_time, time_data, hs_data, bp_data
-    try:
-        val = queue.get_nowait()
-        # logger.info(f'get {val}')
-    except Empty:
-        return
-    time_data.append(time.perf_counter() - start_time)
-    hs_data.append(val)
-    updated_data_x = np.array(time_data)
-    updated_data_y = np.array(hs_data) / 4096.0 * VREF
-    dpg.configure_item('heart_sound', x=updated_data_x, y=updated_data_y)
-    # logger.info('set value successfully')
-    if dpg.get_value('auto_fit_checkbox'):
-        dpg.fit_axis_data('x_axis')
-        # dpg.set_axis_limits_auto('y_axis')
+queue = Queue()
+stop_event = threading.Event()
 
 dpg.create_context()
 
@@ -141,12 +167,16 @@ dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.set_primary_window('primary_window', True)
 
-recv_data_handler = threading.Thread(target=receive_data, args=(queue,),daemon=True)
+recv_data_handler = threading.Thread(target=receive_data, args=(queue, stop_event), daemon=True)
 recv_data_handler.start()
 
-# dpg.start_dearpygui()
-while dpg.is_dearpygui_running():
-    if queue.qsize() > 0:
-        update_plot() # updating the plot directly from the running loop
-    dpg.render_dearpygui_frame()
+update_plot_handler = threading.Thread(target=update_plot, args=(queue,stop_event), daemon=True)
+update_plot_handler.start()
+
+dpg.start_dearpygui()
+# while dpg.is_dearpygui_running():
+#     if queue.qsize() > 0:
+#         update_plot() # updating the plot directly from the running loop
+#     dpg.render_dearpygui_frame()
+stop_event.set()
 dpg.destroy_context()
